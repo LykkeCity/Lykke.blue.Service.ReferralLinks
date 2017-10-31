24179f5ade3f50108e04aeddd57ce4400c136bda
filip-prime <filip.lyubenov@primeholding.com>
2017-10-31 09:40:23 +0200

    comment out BitcoinCoreSettings BitCoin

--- src/Lykke.Service.ReferralLinks.Core/Settings/ServiceSettings/ReferralLinksSettings.cs
+++ src/Lykke.Service.ReferralLinks.Core/Settings/ServiceSettings/ReferralLinksSettings.cs
@@ -3,7 +3,7 @@
     public class ReferralLinksSettings
     {
         public DbSettings Db { get; set; }
-        public BitcoinCoreSettings BitCoin { get; set; }
+        //public BitcoinCoreSettings BitCoin { get; set; }
         public Services Services { get; set; }
         public int ExpirationDaysLimit { get; set; }
         public int ExpiredLinksCheckTimeout { get; set; }
