54e91cd85a6f838738fa47ea85cbf9b47051b38b
Miladin Joksic <miladin.joksic@primeholding.com>
2017-10-31 11:02:00 +0100

    Updated AutoREST client

--- client/Lykke.Service.ReferralLinks.Client/IReferralLinksClient.cs
+++ client/Lykke.Service.ReferralLinks.Client/IReferralLinksClient.cs
@@ -1,7 +1,19 @@
 
+using Lykke.Service.ReferralLinks.Client.AutorestClient.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Lykke.Service.ReferralLinks.Client
 {
     public interface IReferralLinksClient
     {
+        Task<CreateReferralLinkResponse> Create(CreateReferralLinkRequest request);
+        Task<GetReferralLinkResponse> Get(string id);
+        Task<IEnumerable<GetReferralLinkResponse>> GetReferralLinksBySenderIdAndOrStatus(string senderClientId, string state);
+        Task UpdateState(string id, string state);
+        Task<GetReferralLinksStatisticsBySenderIdResponse> GetReferralLinksStatisticsBySenderId(string senderClientId);
+        Task SetUrl(string id, string url);
+        Task<string> ClaimGiftCoins(ClaimGiftCoinsRequest request);
+        Task<string> RequestReferralLink(RequestReferralLinkRequest request);
     }
 }
--- client/Lykke.Service.ReferralLinks.Client/ReferralLinksClient.cs
+++ client/Lykke.Service.ReferralLinks.Client/ReferralLinksClient.cs
@@ -1,23 +1,125 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Common.Log;
+using Lykke.Service.ReferralLinks.Client.AutorestClient;
+using Lykke.Service.ReferralLinks.Client.AutorestClient.Models;
 
 namespace Lykke.Service.ReferralLinks.Client
 {
     public class ReferralLinksClient : IReferralLinksClient, IDisposable
     {
         private readonly ILog _log;
+        private LykkeReferralLinksService _service;
 
         public ReferralLinksClient(string serviceUrl, ILog log)
         {
             _log = log;
+            _service = new LykkeReferralLinksService(new Uri(serviceUrl));
+        }
+
+        public async Task<string> ClaimGiftCoins(ClaimGiftCoinsRequest request)
+        {
+            try
+            {
+                return await _service.ClaimGiftCoinsAsync(request);
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteErrorAsync(nameof(ReferralLinksClient), nameof(ClaimGiftCoins), ex);
+                throw;
+            }
+        }
+
+        public async Task<CreateReferralLinkResponse> Create(CreateReferralLinkRequest request)
+        {
+            throw new NotImplementedException();
         }
 
         public void Dispose()
         {
-            //if (_service == null)
-            //    return;
-            //_service.Dispose();
-            //_service = null;
+            if (_service == null)
+                return;
+            _service.Dispose();
+            _service = null;
+        }
+
+        public async Task<GetReferralLinkResponse> Get(string id)
+        {
+            try
+            {
+                return await _service.GetReferralLinkAsync(id);
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteErrorAsync(nameof(ReferralLinksClient), nameof(Get), ex);
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<GetReferralLinkResponse>> GetReferralLinksBySenderIdAndOrStatus(string senderClientId, string state)
+        {
+            try
+            {
+                return await _service.GetReferralLinksBySenderIdAndOrStatusAsync(senderClientId, state);
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteErrorAsync(nameof(ReferralLinksClient), nameof(GetReferralLinksBySenderIdAndOrStatus), ex);
+                throw;
+            }
+        }
+
+        public async Task<GetReferralLinksStatisticsBySenderIdResponse> GetReferralLinksStatisticsBySenderId(string senderClientId)
+        {
+            try
+            {
+                return await _service.GetReferralLinksStatisticsBySenderIdAsync(senderClientId);
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteErrorAsync(nameof(ReferralLinksClient), nameof(GetReferralLinksStatisticsBySenderId), ex);
+                throw;
+            }
+        }
+
+        public async Task<string> RequestReferralLink(RequestReferralLinkRequest request)
+        {
+            try
+            {
+                return await _service.RequestReferralLinkAsync(request);
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteErrorAsync(nameof(ReferralLinksClient), nameof(RequestReferralLink), ex);
+                throw;
+            }
+        }
+
+        public async Task SetUrl(string id, string url)
+        {
+            try
+            {
+                await _service.SetReferralLinkUrlAsync(id, url);
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteErrorAsync(nameof(ReferralLinksClient), nameof(SetUrl), ex);
+                throw;
+            }
+        }
+
+        public async Task UpdateState(string id, string state)
+        {
+            try
+            {
+                await _service.UpdateReferralLinkStateAsync(id, state);
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteErrorAsync(nameof(ReferralLinksClient), nameof(UpdateState), ex);
+                throw;
+            }
         }
     }
 }
