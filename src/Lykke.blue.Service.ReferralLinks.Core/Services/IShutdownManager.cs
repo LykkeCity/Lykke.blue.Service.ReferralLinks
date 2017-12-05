c102919f67772c58a4aeaf4e58de79c863302f8b
filip-prime <filip.lyubenov@primeholding.com>
2017-12-05 18:54:36 +0200

    delete unused files

--- src/Lykke.blue.Service.ReferralLinks.Core/Services/IShutdownManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Lykke.blue.Service.ReferralLinks.Core.Services
-{
-    public interface IShutdownManager
-    {
-        Task StopAsync();
-    }
-}
--- src/Lykke.blue.Service.ReferralLinks.Core/Settings/ServiceSettings/BitcoinCoreSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace Lykke.blue.Service.ReferralLinks.Core.Settings.ServiceSettings
-//{
-//    //public class BitcoinCoreSettings
-//    //{
-//    //    public string BitcoinCoreApiUrl { get; set; }
-//    //}
-//}
